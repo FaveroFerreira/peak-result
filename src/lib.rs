b41c23eaedd6358d14a637a2d51e27ddcc2fa79e
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2022-10-31 14:41:58 -0300

    add tests
    * test: Add if_ok tests
    
    * test: Add if_err tests

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,63 @@ where
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Peak;
+    // =========================== if_ok() tests ============================
+
+    #[test]
+    #[should_panic]
+    fn test_if_ok_is_called() {
+        Ok::<(), ()>(()).if_ok(|_| panic!("Nice")).unwrap();
+    }
+
+    #[test]
+    #[allow(unused_must_use)]
+    fn test_if_ok_should_not_be_called() {
+        Err::<(), ()>(()).if_ok(|_| panic!("if_ok should not be called"));
+    }
+
+    #[test]
+    fn test_if_ok_uses_result_value() {
+        Ok::<&str, ()>("OK")
+            .if_ok(|e| assert_eq!(e, &"OK"))
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_if_ok_uses_result_value_wrong_assert() {
+        Ok::<&str, ()>("OK").if_ok(|e| assert_eq!(e, &"")).unwrap();
+    }
+
+    // =========================== if_err() tests ===========================
+
+    #[test]
+    #[should_panic]
+    #[allow(unused_must_use)]
+    fn test_if_err_is_called() {
+        Err::<(), ()>(()).if_err(|_| panic!("Nice"));
+    }
+
+    #[test]
+    fn test_if_err_should_not_be_called() {
+        Ok::<(), ()>(())
+            .if_err(|_| panic!("if_err should not be called"))
+            .unwrap();
+    }
+
+    #[test]
+    #[allow(unused_must_use)]
+    fn test_if_err_uses_result_value() {
+        Err::<(), &str>("error").if_err(|e| assert_eq!(e, &"error"));
+    }
+
+    #[test]
+    #[should_panic]
+    #[allow(unused_must_use)]
+    fn test_if_err_uses_result_value_wrong_assert() {
+        Err::<(), &str>("error").if_err(|e| assert_eq!(e, &""));
+    }
+}
